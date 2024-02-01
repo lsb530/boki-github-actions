402e0f9335b7034d8270d54dd1ad155966a730c8
seung.bok <seungbok.lee@huinnoaim.com>
2024-02-01 13:54:02 +0900

    Initial commit

---
+++ src/test/java/com/boki/bokigithubactions/BokiGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boki.bokigithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokiGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
