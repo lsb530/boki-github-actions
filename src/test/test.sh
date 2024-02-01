911fc5025f84af38eaecec3d732c70050580081e
seung.bok <seungbok.lee@huinnoaim.com>
2024-02-01 14:47:16 +0900

    change test script

--- src/test/test.sh
+++ src/test/test.sh
@@ -1,5 +1,6 @@
 #docker run --rm -i -t -v $PWD:/workdir jetbrains/intellij-http-client \
 docker run --rm -v $PWD:/workdir jetbrains/intellij-http-client \
 -D \
-rest.http
+/workdir/rest.http
+#rest.http
 #/workdir/src/test/rest.http
