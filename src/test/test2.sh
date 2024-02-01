2f259619b911557e8c1469ccb951a7fd2fd2ee20
seung.bok <seungbok.lee@huinnoaim.com>
2024-02-01 15:55:22 +0900

    fix ci script

--- src/test/test2.sh
+++ src/test/test2.sh
@@ -1,7 +1,7 @@
 curl -f -L -o ijhttp.zip "https://jb.gg/ijhttp/latest"
 unzip -nq ijhttp.zip
 rm ijhttp.zip
-pwd
-ls
-./ijhttp/ijhttp rest.http
+#pwd
+#ls
+./ijhttp/ijhttp ./src/test/rest.http
 rm -r ijhttp
